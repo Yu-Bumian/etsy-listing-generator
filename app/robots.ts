08e7852d3374b6b9384e12b395837f11f2ad0180
DeminWe <2054559896@qq.com>
2025-12-06 11:57:28 +0800

    Add sitemap add robot

--- app/robots.ts
+++ app/robots.ts
@@ -8,6 +8,6 @@ export default function robots(): MetadataRoute.Robots {
     },
     // 注意：上线后要把这里的域名换成您真实的 Vercel 域名
     // 暂时先写个占位符，或者等拿到域名后再回来改
-    sitemap: 'https://your-domain.vercel.app/sitemap.xml',
+    sitemap: 'https://shoplistingai.com/sitemap.xml',
   }
 }
--- app/sitemap.ts
+++ app/sitemap.ts
@@ -3,7 +3,7 @@ import { MetadataRoute } from 'next'
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: 'https://your-domain.vercel.app', // 上线后记得换成真实域名
+      url: 'https://shoplistingai.com', // 上线后记得换成真实域名
       lastModified: new Date(),
       changeFrequency: 'weekly',
       priority: 1,
